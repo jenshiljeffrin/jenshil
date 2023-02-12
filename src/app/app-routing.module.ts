952d8b996395f9809343cfdf2a5513ce1861cd2c
jenshiljeffrin <jenshiljenshil37@gmail.com>
2023-02-12 11:40:33 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { NoPageComponent } from './no-page/no-page.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component:AboutComponent,
+    path:"about"
+  },
+  {
+    component:UserComponent,
+    path:'user/:id'
+  },
+  {
+  component:HomeComponent,
+  path:""
+  },
+  {
+    component:NoPageComponent,
+    path:"**"
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
